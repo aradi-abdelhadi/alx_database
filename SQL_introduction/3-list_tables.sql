cb3865d135632aec4392f9ac6b1b1b3d0dc97fd0
aradi abdelhadi <ard.abdelhadi@gmail.com>
2024-01-11 10:27:28 -0800

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,11 +1,12 @@
 -- This script lists all the tables of a database in my MySQL server.
 
-CREATE DATABASE holbteron_0;
+-- Create the new database
+CREATE DATABASE IF NOT EXISTS holbteron_0;
 USE holbteron_0;
 
 -- Create tables
-CREATE TABLE holbteron_1 (id INT, name VARCHAR(50));
-CREATE TABLE holbteron_2 (id INT, data VARCHAR(50));
+CREATE TABLE IF NOT EXISTS holbteron_1 (id INT, name VARCHAR(50));
+CREATE TABLE IF NOT EXISTS holbteron_2 (id INT, data VARCHAR(50));
 
 -- List tables
 SHOW TABLES;
