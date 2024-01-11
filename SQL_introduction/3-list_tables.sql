dd0563b891fda89bff8371ea0b2bef517dd225bb
aradi abdelhadi <ard.abdelhadi@gmail.com>
2024-01-11 15:44:06 -0800

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,12 +1,4 @@
 -- This script lists all the tables of a database in my MySQL server.
 
--- Create the new database
-CREATE DATABASE IF NOT EXISTS holbteron_0;
-USE holbteron_0;
 
--- Create tables
-CREATE TABLE IF NOT EXISTS holbteron_1 (id INT, name VARCHAR(50));
-CREATE TABLE IF NOT EXISTS holbteron_2 (id INT, data VARCHAR(50));
-
--- List tables in the current database
-SHOW TABLES;
+SHOW TABLES FROM mysql;
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,9 +1,6 @@
 -- This script creates a table called first_table in the current database in my mySQL server.
 
-USE hbtn_0c_0;
-SELECT DATABASE();
-
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
+  id INT,
+  name VARCHAR(256)
 );
